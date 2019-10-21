b65c59d171df16a74d654e267cfe4da40872f6d0
Julien Cresus <cresusjulien@gmail.com>
2019-10-21 23:27:53 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0d637822f4c3bf65fd0cf0b24e60a8a2.js"
+  "/precache-manifest.6f5f97f82bf04cc72c100b023ab509f6.js"
 );
 
 workbox.clientsClaim();
