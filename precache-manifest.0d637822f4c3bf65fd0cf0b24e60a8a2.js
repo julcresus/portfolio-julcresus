2a128840d82ea295af49dd3279da6bfb52bf34ec
Julien Cresus <cresusjulien@gmail.com>
2019-10-21 23:26:14 +0100

    Updates

--- precache-manifest.0d637822f4c3bf65fd0cf0b24e60a8a2.js
+++ precache-manifest.0d637822f4c3bf65fd0cf0b24e60a8a2.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.42ac5946.js"
   },
   {
-    "revision": "b158b11ee95e7d1bd798",
-    "url": "/static/js/main.b158b11e.chunk.js"
+    "revision": "f2a06daa93ecf7dc1e89",
+    "url": "/static/js/main.f2a06daa.chunk.js"
   },
   {
     "revision": "c86d2bccb494cd6416ea",
     "url": "/static/js/2.c86d2bcc.chunk.js"
   },
   {
-    "revision": "b158b11ee95e7d1bd798",
+    "revision": "f2a06daa93ecf7dc1e89",
     "url": "/static/css/main.2572ce13.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/static/css/2.cef35c2b.chunk.css"
   },
   {
-    "revision": "cc819e25af662a2bfa07a637a5696af7",
+    "revision": "1ca246bc29269be765b3d2c62692e5e3",
     "url": "/index.html"
   }
 ];
