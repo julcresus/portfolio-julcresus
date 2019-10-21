5c6b76a6cd4b4352e3507cf918406cbdd0d4e1a9
Julien Cresus <cresusjulien@gmail.com>
2019-10-21 17:48:38 +0100

    Updates

--- precache-manifest.c6bede17dd501dc056bc8da79dda9fbd.js
+++ precache-manifest.c6bede17dd501dc056bc8da79dda9fbd.js
@@ -4,23 +4,23 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.42ac5946.js"
   },
   {
-    "revision": "b158b11ee95e7d1bd798",
-    "url": "/static/js/main.b158b11e.chunk.js"
+    "revision": "97e5e4721e7d5e977723",
+    "url": "/static/js/main.97e5e472.chunk.js"
   },
   {
     "revision": "c86d2bccb494cd6416ea",
     "url": "/static/js/2.c86d2bcc.chunk.js"
   },
   {
-    "revision": "b158b11ee95e7d1bd798",
-    "url": "/static/css/main.2572ce13.chunk.css"
+    "revision": "97e5e4721e7d5e977723",
+    "url": "/static/css/main.d532eeb3.chunk.css"
   },
   {
     "revision": "c86d2bccb494cd6416ea",
     "url": "/static/css/2.cef35c2b.chunk.css"
   },
   {
-    "revision": "f72c86947d1f4af7fa5df377500ae4b5",
+    "revision": "94e06b7bf236ed28831624b8a1e01556",
     "url": "/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f95d3258010972e4b5cafcaf17ce6915.js"
+  "/precache-manifest.c6bede17dd501dc056bc8da79dda9fbd.js"
 );
 
 workbox.clientsClaim();
