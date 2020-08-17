ab5bb311bfb663769b2cdaf6d734a6a0f4f81eb9
Julien Cresus <cresusjulien@gmail.com>
2020-08-17 03:21:14 +0100

    Updates

---
+++ precache-manifest.bfb49571d07dceb1f30671ef1c66cd2b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f47bbf5f3b8192d0271d88a54d6ff13b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "35548347157e956be9b1",
+    "url": "/static/css/2.fb346933.chunk.css"
+  },
+  {
+    "revision": "60f994ea394fea96df74",
+    "url": "/static/css/main.24d1ff5e.chunk.css"
+  },
+  {
+    "revision": "35548347157e956be9b1",
+    "url": "/static/js/2.0fd47676.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.0fd47676.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60f994ea394fea96df74",
+    "url": "/static/js/main.fe810571.chunk.js"
+  },
+  {
+    "revision": "1c8c04924c4d241f0b91",
+    "url": "/static/js/runtime-main.1d221799.js"
+  }
+]);
